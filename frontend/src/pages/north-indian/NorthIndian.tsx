2be2749b07031ea1e739a75256fc6580572f9f2a
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2024-12-12 05:22:36 +0530

    Basic changes and fixing the bugs

--- frontend/src/pages/north-indian/NorthIndian.tsx
+++ frontend/src/pages/north-indian/NorthIndian.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuItems from '../../components/MenuItems'
 import { NorthIndianFood } from './data/NorthFoodData';
 import Footer from '../../components/Footer';
@@ -13,14 +13,28 @@ export interface northFoodTypes {
 }[];
 
 const NorthIndian: React.FC = () => {
+
+    const [category, filterCategory] = useState("All")
+    const filterFoodData = category === "All" ? NorthIndianFood : NorthIndianFood.filter((items) => items.category === category);
+
     return (
         <div className='bg-[url("images/background.png")]'>
             <div className='my-6 px-3 md:px-20'>
-                <h2 className='text-xl md:text-3xl font-semibold '>North-Indian Main Course</h2>
-
+                <div className='flex space-x-5 items-center'>
+                    <h2 className='text-xl md:text-3xl font-semibold'>South-Indian Special</h2>
+                    <select
+                        className='border border-black rounded-lg py-1 px-2 outline-none w-auto font-semibold'
+                        value={category}
+                        onChange={(e) => filterCategory(e.target.value)}
+                    >
+                        <option value="All">All</option>
+                        <option value="Veg">Veg</option>
+                        <option value="Non-Veg">Non-Veg</option>
+                    </select>
+                </div>
                 <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-6">
 
-                    {NorthIndianFood.map((northFood: northFoodTypes, index: number) => (
+                    {filterFoodData.map((northFood: northFoodTypes, index: number) => (
                         <MenuItems key={index}
                             name={northFood.name}
                             description={northFood.description}
@@ -31,7 +45,7 @@ const NorthIndian: React.FC = () => {
                     ))}
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
--- frontend/src/pages/south-indian/SouthIndian.tsx
+++ frontend/src/pages/south-indian/SouthIndian.tsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import MenuItems from '../../components/MenuItems'
+import React, { useState } from 'react';
+import MenuItems from '../../components/MenuItems';
 import { southIndianFood } from './data/SouthFood';
 import Footer from '../../components/Footer';
-
 
 export interface southFoodTypes {
     name: string;
     image: string;
     price: number;
     description: string;
-    category: string
+    category: string;
 }[];
 
 const NorthIndian: React.FC = () => {
+    const [category, setCategory] = useState("All");
+
+    const filterFoodData = category === "All" ? southIndianFood : southIndianFood.filter((food) => food.category === category);
+
     return (
         <div className='bg-[url("images/background.png")]'>
-            <div className='my-6 px-3 md:px-20 '>
-                <h2 className='text-xl md:text-3xl font-semibold '>South-Indian Special</h2>
+            <div className='my-6 px-3 md:px-20'>
+                <div className='flex space-x-5 items-center'>
+                    <h2 className='text-xl md:text-3xl font-semibold'>South-Indian Special</h2>
+                    <select
+                        className='border border-black rounded-lg py-1 px-2 outline-none w-auto font-semibold'
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option value="All">All</option>
+                        <option value="Veg">Veg</option>
+                        <option value="Non-Veg">Non-Veg</option>
+                    </select>
+                </div>
 
+                {/* Food Items */}
                 <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-6">
-
-                    {southIndianFood.map((northFood: southFoodTypes, index: number) => (
-                        <MenuItems key={index}
-                            name={northFood.name}
-                            description={northFood.description}
-                            image={northFood.image}
-                            price={northFood.price}
+                    {filterFoodData.map((food: southFoodTypes, index: number) => (
+                        <MenuItems
+                            key={index}
+                            name={food.name}
+                            description={food.description}
+                            image={food.image}
+                            price={food.price}
                         />
-
                     ))}
                 </div>
             </div>
             <Footer />
         </div>
-    )
-}
+    );
+};
 
-export default NorthIndian
+export default NorthIndian;
